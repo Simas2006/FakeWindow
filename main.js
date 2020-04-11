b10184e2f9ab32ea77716169aeefc09aec565902
Simas2006 <sjuknelis@gmail.com>
2020-04-11 17:57:56 -0400

    Wrap into electron + move image from background into object

---
+++ main.js
@@ -0,0 +1,29 @@
+var {app,BrowserWindow,globalShortcut,Menu} = require("electron");
+var fs = require("fs");
+var window;
+
+function createWindow() {
+  var size = require("electron").screen.getPrimaryDisplay().size;
+  window = new BrowserWindow({
+    width: size.width,
+    height: size.height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  //window.webContents.openDevTools();
+  window.on("closed",function() {
+    window = null;
+  });
+  window.loadURL(`file://${__dirname}/static/index.html`);
+}
+
+app.on("ready",createWindow);
+
+app.on("window-all-closed",function() {
+  app.quit();
+});
+
+app.on("activate",function() {
+  if ( ! window ) createWindow();
+});
--- static/script.js
+++ static/script.js
@@ -1,9 +1,12 @@
+var image;
+
 function displayURLFromTime() {
   var pad = n => n < 10 ? "0" + n : n;
   var d = new Date();
-  document.body.style.backgroundImage = `url("./images/${pad(d.getHours())}_${pad(d.getMinutes())}_00.jpg")`;
+  image.src = `./images/${pad(d.getHours())}_${pad(d.getMinutes())}_00.jpg`;
 }
 
 window.onload = function() {
+  image = document.getElementById("image");
   setInterval(displayURLFromTime,500);
 }
