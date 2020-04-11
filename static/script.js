ce2c24c49b12ccf1d3c3ce6d6d82da5cd78570a5
Simas2006 <sjuknelis@gmail.com>
2020-04-11 19:03:19 -0400

    Update on-screen time and date

--- static/script.js
+++ static/script.js
@@ -1,9 +1,22 @@
-var image;
+var fs = require("fs");
+var image,currentLink;
+var daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 function displayURLFromTime() {
   var pad = n => n < 10 ? "0" + n : n;
   var d = new Date();
-  image.src = `./images/${pad(d.getHours())}_${pad(d.getMinutes())}_00.jpg`;
+  var nextLink = `${__dirname}/../images/${pad(d.getHours())}_${pad(d.getMinutes())}_00.jpg`;
+  if ( currentLink == nextLink ) return;
+  currentLink = nextLink;
+  var ampm = (d.getHours() >= 12 ? "P" : "A");
+  var readableHours = (ampm == "A" ? d.getHours() : d.getHours() - 12) || 12;
+  document.getElementById("timeText").innerText = `${readableHours}:${pad(d.getMinutes())} ${ampm}M`;
+  document.getElementById("dateText").innerText = `${daysOfWeek[d.getDay()]}, ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+  fs.stat(nextLink,function(err,data) {
+    if ( err && err.code == "ENOENT" ) return;
+    image.src = nextLink;
+  });
 }
 
 window.onload = function() {
